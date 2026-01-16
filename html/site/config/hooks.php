a164991f535dd2143000381f19582633a08bb096
Philip Marnef <philip@bureaumarnef.be>
2026-01-16 13:53:11 +0100

    Testing new 'updated' info fields hook

--- html/site/config/hooks.php
+++ html/site/config/hooks.php
@@ -4,15 +4,21 @@ return [
 
 	'page.update:after' => function ( Kirby\Cms\Page $newPage, Kirby\Cms\Page $oldPage ) {
 
-		// update 'updated_on' field
-		$update = [
-			'updated_on' => date('Y-m-d H:i:s'),
-		];
+		// only update the meta fields on pages that have them
+		if ( $newPage->updated_on()->exists() && $newPage->updated_by()->exists() ) {
 
-		// update 'updated_by' field
-		if ( $user = $this->user() ) $updated['updated_by'] = $user->uuid()->toString();
+			$update ??= [];
 
-		$newPage->update($update);
+			// update 'updated_on' field
+			$update['updated_on'] = date('Y-m-d H:i:s');
+
+			// update 'updated_by' field
+			if ( $user = $this->user() ) $update['updated_by'] = $user->uuid()->toString();
+
+			// use $page->save() to prevent an update hook loop
+			$newPage = $newPage->save($update);
+
+		}
 
 	}
 
